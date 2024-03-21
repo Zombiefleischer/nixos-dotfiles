0d56255b84cf8512565ee3c663014293630c64f2
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-03-21 23:11:36 +0100

    14 current  2024-03-21 23:11:33  23.11.5441.614b4613980a  6.1.82                          *

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,15 @@
     ];
   };
 
+  # Activate protonmail bridge
+  systemd.user.services.protonmail-bridge = {
+    description = "Protonmail Bridge";
+    enable = true;
+    script = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --noninteractive --log-level info";
+    wantedBy = ["graphical-session.target"];
+    partOf = ["graphical-session.target"];
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
