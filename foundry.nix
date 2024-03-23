c1644128500a51b12ecc1264179a173b65a96f23
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-03-23 16:26:32 +0100

    23 current  2024-03-23 16:18:48  24.05.20240319.b06025f   6.6.22                          *

--- foundry.nix
+++ foundry.nix
@@ -6,19 +6,10 @@
   services.foundryvtt = {
     enable = true;
     hostName = "zombiefleischer.ddns.net";
-    proxySSL = true;
-    proxyPort = 443;
     dataDir = "/home/zombiefleischer/.local/share/Foundry_V11_plut";
     package = pkgs.foundryvtt.packages.x86_64-linux.foundryvtt_11.overrideAttrs {
       version = "11.0.0+315";
     };
-  };
-  services.caddy = {
-    enable = true;
-
-    virtualHosts."foundry.zombiefleischer.ddns.net".extraConfig = ''
-      reverse_proxy localhost:30000
-    '';
   };
   networking.firewall = {
     enable = true;
