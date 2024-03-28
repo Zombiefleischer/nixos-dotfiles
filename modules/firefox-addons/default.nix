87b979552d9efb81d238636b8fcfc1c6c3ea7844
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-03-28 11:51:11 +0100

    81 current  2024-03-28 11:51:04  24.05.20240325.57e6b3a   6.6.22                          *

--- modules/firefox-addons/default.nix
+++ modules/firefox-addons/default.nix
@@ -1,7 +1,5 @@
 {pkgs, ...}: {
   # Enable Firefox Plasma Browser Integration
-  nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
-
   programs.firefox.nativeMessagingHosts.packages = with pkgs; [
     plasma-browser-integration
   ];
