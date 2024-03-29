5e83d638246bf486add610c61c2e67956dc2e7ae
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-03-29 14:52:17 +0100

    87 current  2024-03-29 14:42:54  24.05.20240327.2726f12   6.6.22                          *

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -45,9 +45,9 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  # hardware.opengl = {
-  # enable = true;
-  # driSupport = true;
-  # driSupport32Bit = true;
-  # };
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
 }
