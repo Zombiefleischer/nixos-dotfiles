27c8d1f13feb9dbd0690cfea841f7d1e4d81c062
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-03-29 17:55:57 +0100

    89 current  2024-03-29 17:55:51  24.05.20240327.2726f12   6.6.22                          *

--- flake.nix
+++ flake.nix
@@ -39,6 +39,7 @@
       specialArgs = {inherit inputs system;};
       modules = [
         ./configuration.nix # plasma6/Wayland
+        ./drv/wallpaper-engine-kde.nix
         ./home/flatpak
         ./modules/bindfs
         ./modules/firefox-addons
