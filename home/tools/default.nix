a5452aa18d619e956ecc511c746e84bf7c349f7b
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-03-30 15:02:15 +0100

    95 current  2024-03-30 15:02:08  24.05.20240329.d8fe5e6   6.6.23                          *

--- home/tools/default.nix
+++ home/tools/default.nix
@@ -23,10 +23,7 @@
     telegram-desktop
     thunderbird
     tidal-hifi
-    (makeAutostartItem {
-      name = "vesktop";
-      package = vesktop;
-    })
+    vesktop
     vivaldi
     wacomtablet
   ];
