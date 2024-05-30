51f56124b032580c95bca451ef4fca8ee341be7c
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-05-30 19:33:11 +0200

    144 current  2024-05-30 19:33:06  24.11.20240529.ad57eef  6.6.32                          *

--- modules/obs/default.nix
+++ modules/obs/default.nix
@@ -2,6 +2,7 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [
     v4l2loopback
   ];
+  boot.kernelModules = ["v4l2loopback"];
   boot.extraModprobeConfig = ''
     options v4l2loopback devices=1 video_nr=63 card_label="OBS Cam" exclusive_caps=1
   '';
