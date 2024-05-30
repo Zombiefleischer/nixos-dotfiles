ca7bdda78ab90c7872bb38b128ecda91b5873993
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-05-30 22:42:59 +0200

    145 current  2024-05-30 22:42:48  24.11.20240529.ad57eef  6.6.32                          *

--- home/obs/default.nix
+++ home/obs/default.nix
@@ -3,7 +3,10 @@
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
       obs-backgroundremoval
+      obs-composite-blur
       obs-pipewire-audio-capture
+      obs-shaderfilter
+      obs-vintage-filter
       # wlrobs
     ];
   };
