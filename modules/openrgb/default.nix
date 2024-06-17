db02cdb1e73073b874403abec0be6cc8ad0550da
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-06-17 10:11:05 +0200

    162 current  2024-06-17 10:10:59  24.11.20240607.051f920  6.6.32                          *

--- modules/openrgb/default.nix
+++ modules/openrgb/default.nix
@@ -1,13 +1,8 @@
-{pkgs, ...}: let
-  openrgb-rules = builtins.fetchurl {
-    url = "https://gitlab.com/CalcProgrammer1/OpenRGB/-/raw/master/60-openrgb.rules";
-  };
-in {
+{pkgs, ...}: {
   services.hardware.openrgb = {
     enable = true;
     package = pkgs.openrgb-with-all-plugins;
     motherboard = "amd";
   };
   boot.kernelModules = ["i2c-dev" "i2c-piix4"];
-  services.udev.extraRules = builtins.readFile openrgb-rules;
 }
