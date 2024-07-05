7a15ea94c41ba3aaec217fa4a6e0f6b361979054
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-07-05 19:24:33 +0200

    176 current  2024-06-30 12:21:23  24.11.20240627.b2852eb  6.6.36                          *

--- drv/libx52.nix
+++ drv/libx52.nix
@@ -1,9 +1,9 @@
 {
+  lib,
   stdenv,
-  fetchFromGitHub,
+  fetchurl,
   automake,
   autoconf,
-  autopoint,
   gettext,
   hidapi,
   libtool,
@@ -11,32 +11,35 @@
   libevdev,
   pkg-config,
   python3,
+  doxygen,
+  cmocka,
+  git,
 }:
 stdenv.mkDerivation rec {
   pname = "libx52";
-  version = "1.0.0";
-
-  src = fetchFromGitHub {
-    owner = "nirenjan";
-    repo = "libx52";
-    rev = "master";
-    sha256 = "0wvh5s4g5gn4nxlrbf5sq5p7spygkfj6mri2z7w1vjhx2vrbm2ja";
+  version = "0.3.2";
+  src = fetchurl {
+    url = "https://github.com/nirenjan/libx52/releases/download/v${version}/${pname}_${version}.orig.tar.gz";
+    sha256 = "c4d4cdce7f2fce85871f3b1a4bf0f8122eb2969e62ee0d381984c9d26e0a8e25";
   };
 
-  nativeBuildInputs = [automake autoconf autopoint gettext libtool pkg-config python3];
+  nativeBuildInputs = [automake autoconf libtool pkg-config python3 gettext doxygen cmocka git];
   buildInputs = [hidapi libusb1 libevdev];
 
-  preConfigure = ''
-    ./autogen.sh
+  configurePhase = ''
+    cd ${src}/build
+    ../configure --prefix=$out --localstatedir=/var --sysconfdir=/etc --disable-silent-rules --disable-maintainer-mode
   '';
 
-  configureFlags = [
-    "--prefix=/usr"
-    "--localstatedir=/var"
-    "--sysconfdir=/etc"
-  ];
+  buildPhase = "make";
 
-  meta = with stdenv.lib; {
+  installPhase = ''
+    make install DESTDIR=$out
+    rmdir $out/var/run
+    rmdir $out/var/log
+  '';
+
+  meta = with lib; {
     description = "Driver for the Saitek X52 and X52 Pro HOTAS Joystick.";
     homepage = "https://github.com/nirenjan/libx52";
     license = licenses.gpl2;
