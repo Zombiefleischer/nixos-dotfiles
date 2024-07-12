0a886532b5e0b4f0925a788d1e9d1112ca134502
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-07-12 22:54:06 +0200

    181 current  2024-07-12 22:38:18  24.11.20240703.9f4128e  6.6.36                          *

--- home/desktop-files/edhm-ui.nix
+++ home/desktop-files/edhm-ui.nix
@@ -1,7 +1,7 @@
 {...}: {
   xdg.desktopEntries.edhm-ui = {
     name = "EDHM UI";
-    Comment = "E:D HUD Mod UI";
+    comment = "E:D HUD Mod UI";
     exec = "env WINEDEBUG='-all' WINEPREFIX='/home/zombiefleischer/.wine_edhmui' '/home/zombiefleischer/.wine_edhmui/drive_c/users/zombiefleischer/AppData/Local/EDHM_UI/EDHM_UI_mk2.exe'";
     icon = "/home/zombiefleischer/.wine_edhmui/logo.png";
     terminal = "false";
