89cfa8499d99f083a58eabe4559157ba0e36b447
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-08-17 00:57:54 +0200

    224 current  2024-08-17 00:57:47  24.11.20240814.c3aa7b8  6.10.4                          *

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
       modules = [
         ./machines/Leviathan/configuration.nix # plasma6/Wayland
         # ./drv/libx52.nix # not working atm
-        ./drv/wallpaper-engine-kde.nix
+        ./machines/Leviathan/drv/wallpaper-engine-kde.nix
         ./machines/Leviathan/modules/flatpak
         ./machines/Leviathan/modules/bindfs
         ./machines/Leviathan/modules/firefox-addons
