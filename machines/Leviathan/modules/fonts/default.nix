b62010f5ae94cdead4ff26c6998f4988a5948083
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-10-29 09:33:35 +0100

    296 current  2024-10-29 09:33:33  24.11.20241025.18536bf  6.11.5                          *

--- machines/Leviathan/modules/fonts/default.nix
+++ machines/Leviathan/modules/fonts/default.nix
@@ -7,9 +7,14 @@
     mkdir -p $out/share/fonts
     cp ${./Pentiment}/* $out/share/fonts/
   '';
+  ElectricPanda = pkgs.runCommand "electric-panda" {buildInputs = [pkgs.fontconfig];} ''
+    mkdir -p $out/share/fonts
+    cp ${./ElectricPanda}/* $out/share/fonts/
+  '';
 in {
   fonts.packages = with pkgs; [
     PentimentFonts
+    ElectricPanda
   ];
 }
 # TODO: Add other fonts in here
