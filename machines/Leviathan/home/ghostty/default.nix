c889d3cdd70055cfe6bb7fe00622144fc85ff6d2
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-12-30 09:37:08 +0100

    319 current  2024-12-24 11:48:26  25.05.20241217.d3c42f1  6.12.5                          *

---
+++ machines/Leviathan/home/ghostty/default.nix
@@ -0,0 +1,22 @@
+{
+  inputs,
+  system,
+  ...
+}: {
+  programs.ghostty = {
+    enable = true;
+    package = inputs.ghostty-package.${system}.default;
+    shellIntegration = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    settings = {
+      background-opacity = 0.8;
+      background-blur-radius = 0;
+
+      font-family = "FiraCode Nerd Font Ret";
+      font-size = 14;
+    };
+  };
+}
