2e92637f6a50f954949f4dce5ce88490ba550ebb
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2024-12-30 10:01:46 +0100

    321 current  2024-12-30 10:01:45  25.05.20241217.d3c42f1  6.12.5                          *

--- machines/Leviathan/home/ghostty/default.nix
+++ machines/Leviathan/home/ghostty/default.nix
@@ -12,11 +12,16 @@
     };
 
     settings = {
-      background-opacity = 0.8;
+      background-opacity = 0.75;
       background-blur-radius = 0;
 
       font-family = "FiraCode Nerd Font Ret";
       font-size = 14;
+
+      theme = "catppuccin-macchiato";
+
+      window-theme = "dark";
+      window-decoration = false;
     };
   };
 }
