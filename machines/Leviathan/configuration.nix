7a0de73d271b71e292361c6da086fb9adaacb196
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2025-01-11 16:47:24 +0100

    Revert "330 current  2025-01-11 16:46:59  25.05.20250108.bffc22e  6.12.8                          *"
    This reverts commit cd2b23ee5a792afc5c8535958fded70ce33e7ce3.

--- machines/Leviathan/configuration.nix
+++ machines/Leviathan/configuration.nix
@@ -36,7 +36,7 @@
   networking.nameservers = ["192.168.178.34"];
 
   # Enable Bluetooth
-  hardware.bluetooth.enable = false;
+  hardware.bluetooth.enable = true;
   hardware.bluetooth.settings = {
     General = {
       Experimental = true;
