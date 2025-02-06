edb4a803edc39f13aba72d76deec3803d69134fb
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2025-02-06 11:38:03 +0100

    342 current  2025-02-06 11:38:02  25.05.20250204.799ba5b  6.13.1                          *

--- machines/Leviathan/configuration.nix
+++ machines/Leviathan/configuration.nix
@@ -67,7 +67,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.enable = false;
   services.desktopManager.plasma6.enable = true;
   services.displayManager.defaultSession = "plasma";
   programs.xwayland.enable = true;
