2e9e83504e1cf05fb272dcb7297e66472907c596
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2025-02-12 17:58:16 +0100

    94 current  2025-02-12 17:58:14  25.05.20250208.a79cfe0  6.13.1                          *

--- machines/Cthulhu/home/cursor/default.nix
+++ machines/Cthulhu/home/cursor/default.nix
@@ -6,4 +6,11 @@
     gtk.enable = true;
     x11.enable = true;
   };
+
+  home.sessionVariables = {
+    HYPRCURSOR_THEME = "Posy_Cursor_Black_HC";
+    HYPRCURSOR_SIZE = 64;
+    XCURSOR_THEME = "Posy_Cursor_Black";
+    XCURSOR_SIZE = 64;
+  };
 }
--- machines/Cthulhu/home/hyprland/default.nix
+++ machines/Cthulhu/home/hyprland/default.nix
@@ -51,6 +51,16 @@
         # "/home/zombiefleischer/scripts/swww_randomize_multi.sh /home/zombiefleischer/Pictures/i3_wallpapers"
       ];
 
+      env = [
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        "QT_QPA_PLATFORM,wayland;xcb"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "QT_QPA_PLATFORMTHEME,qt5ct"
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+      ];
+
       # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
       input = {
         kb_layout = "us";
