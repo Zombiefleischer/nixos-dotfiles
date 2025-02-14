0a48cc339f123cd7644a2085a843e5eaa7aaf2e1
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2025-02-14 09:58:12 +0100

    99 current  2025-02-14 09:58:10  25.05.20250208.a79cfe0  6.13.1                          *

--- machines/Cthulhu/modules/display-manager/default.nix
+++ machines/Cthulhu/modules/display-manager/default.nix
@@ -7,10 +7,6 @@
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --theme 'border=magenta;text=cyan;prompt=green;time=cyan;action=blue;button=yellow;container=black;input=red' --time --time-format '%H:%M | %a • %h | 1%F' --cmd Hyprland";
         user = "greeter";
       };
-      initial_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --theme 'border=magenta;text=cyan;prompt=green;time=cyan;action=blue;button=yellow;container=black;input=red' --time --time-format '%H:%M | %a • %h | 1%F' --cmd Hyprland";
-        user = "zombiefleischer";
-      };
     };
   };
 }
