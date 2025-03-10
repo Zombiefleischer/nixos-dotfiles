c1ca96ec6e9ed7d7c2ad428a621ae1953fedc869
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2025-03-10 10:04:58 +0100

    367 current  2025-03-10 10:04:56  25.05.20250303.ba487db  6.13.5                           *

--- machines/Leviathan/modules/wireguard/default.nix
+++ machines/Leviathan/modules/wireguard/default.nix
@@ -1,5 +1,8 @@
 _: {
-  networking.wg-quick.interfaces.wg0.configFile = "/home/zombiefleischer/Documents/PdH/wg0.conf";
+  networking.wg-quick.interfaces.wg0 = {
+    configFile = "/home/zombiefleischer/Documents/PdH/wg0.conf";
+    autostart = false;
+  };
 
   networking.networkmanager.dns = "systemd-resolved";
   services.resolved.enable = true;
