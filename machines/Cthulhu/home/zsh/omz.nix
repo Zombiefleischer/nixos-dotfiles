40e60edd5cdca732fbe79c4dd0ef71aec1ec9f5d
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2025-03-20 10:21:49 +0100

    120 current  2025-03-20 10:21:47  25.05.20250318.b6eaf97  6.13.7                          *

--- machines/Cthulhu/home/zsh/omz.nix
+++ machines/Cthulhu/home/zsh/omz.nix
@@ -58,35 +58,19 @@
   programs.zsh.plugins = [
     {
       name = "fzf-tab";
-      src = pkgs.fetchFromGitHub {
-        owner = "Aloxaf";
-        repo = "fzf-tab";
-        rev = "6aced3f35def61c5edf9d790e945e8bb4fe7b305";
-        sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-      };
+      src = "${pkgs.zsh-fzf-tab}/share/fzf-tab";
     }
     {
       name = "nix-zsh-completions";
-      file = "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
-      src = "${pkgs.nix-zsh-completions}/share/zsh/plugins";
+      src = "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
     }
     {
       name = "you-should-use";
-      src = pkgs.fetchFromGitHub {
-        owner = "MichaelAquilina";
-        repo = "zsh-you-should-use";
-        rev = "56616de037082f7dc0a143eb244ea27e5a697ef9";
-        sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-      };
+      src = "${pkgs.zsh-you-should-use}/share/zsh/plugins/you-should-use";
     }
     {
-      name = "zsh-bat";
-      src = pkgs.fetchFromGitHub {
-        owner = "fdellwing";
-        repo = "zsh-bat";
-        rev = "467337613c1c220c0d01d69b19d2892935f43e9f";
-        sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-      };
+      name = "nix-shell";
+      src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
     }
   ];
 }
