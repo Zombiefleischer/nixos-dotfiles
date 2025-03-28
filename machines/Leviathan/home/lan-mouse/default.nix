c50d2c769ad0c492a76070c1a2680c5257b558eb
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2025-03-28 10:35:18 +0100

    383 current  2025-03-28 10:35:16  25.05.20250318.b6eaf97  6.13.7                          *

--- machines/Leviathan/home/lan-mouse/default.nix
+++ machines/Leviathan/home/lan-mouse/default.nix
@@ -6,7 +6,6 @@
     systemd = true;
 
     settings = {
-      port = 4242;
       authorized_fingerprints = {
         "cf:1b:dd:73:db:5d:a9:87:ce:0e:02:60:c4:f7:99:77:95:69:7d:27:ce:66:d6:c6:a6:9a:5b:f7:31:c9:f8:d4" = "Cthulhu";
       };
