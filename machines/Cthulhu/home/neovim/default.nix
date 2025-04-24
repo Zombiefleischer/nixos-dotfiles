fb6db4c30cd7daec7b12cb985ec2d2c0375c8a38
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2025-04-24 22:54:27 +0200

    394 current  2025-04-24 22:54:26  25.05.20250421.c11863f  6.14.3                          *

--- machines/Cthulhu/home/neovim/default.nix
+++ machines/Cthulhu/home/neovim/default.nix
@@ -41,7 +41,7 @@ in {
           enable = true;
           event = ["TextYankPost"];
           pattern = ["*"];
-          command = "silent! lua vim.hl.on_yank {higroup='Visual', timeout=300}";
+          command = "silent! lua vim.hl.on_yank {higroup='Search', timeout=300}";
         }
       ];
 
