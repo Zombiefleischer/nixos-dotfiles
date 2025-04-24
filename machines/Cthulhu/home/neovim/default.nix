b984fc267de44728e17ad34e209af2982524fbe2
Zombiefleischer <zombiefleischer@zombiefleischer.de>
2025-04-24 17:59:50 +0200

    155 current  2025-04-24 17:59:48  25.05.20250421.c11863f  6.14.3                          *

--- machines/Cthulhu/home/neovim/default.nix
+++ machines/Cthulhu/home/neovim/default.nix
@@ -36,15 +36,15 @@ in {
       };
 
       autocmds = [
-        {
-          desc = "Highlight yanked lines";
-          enable = true;
-          event = ["TextYankPost"];
-          pattern = ["*"];
-          callback = mkLuaInline ''
-            silent! lua vim.hl.on_yank {higroup='Visual', timeout=300}
-          '';
-        }
+#        {
+#          desc = "Highlight yanked lines";
+#          enable = true;
+#          event = ["TextYankPost"];
+#          pattern = ["*"];
+#          callback = mkLuaInline ''
+#	    silent! lua vim.hl.on_yank {higroup='Visual', timeout=300}
+#	  '';
+#        }
       ];
 
       keymaps = [
